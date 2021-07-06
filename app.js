d6e19a65ba64e1837f39206c4bc3c63fdff09cb0
StasTikh <82860582+StasTikh@users.noreply.github.com>
2021-07-06 16:05:25 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,18 +1,48 @@
-function guessSearch() {
-    let startSearch = 0;
-    let endSearch = 100;
-
-    while (startSearch <= endSearch) {
-        let answerSearch = ((endSearch-startSearch)/2) + startSearch;
-
-        let tryA = prompt(`Is your answer ${answerSearch}? If yes, press "y", if it's smaller press "s", if bigger press "b"`);
+(function(){
+    let button = document.getElementById("btn");
+    let answerF = document.getElementById("answerField");
+    let small = document.getElementById("small");
+    let big = document.getElementById("big");
+    let win = document.getElementById("win");
+    let restart = document.getElementById("restartGame");  
+    let numGuess = 0;
+    let low = 0;
+    let high = 101;
+    let tryA = Math.floor( (low + high)/2 );
+  
+    button.addEventListener("click", function(){
+      answerF.innerHTML = tryA;
+      button.style.display = "none";
+      small.style.display = "block";
+      big.style.display = "block";
+      win.style.display = "block";
+    }); 
         
-        if (tryA == "y") {
-            alert("Bingo!");
-        } else if (tryA == "s") {
-            startSearch = answerSearch;
-        } else if (tryA == "b") {
-            endSearch = answerSearch;
-        }
-    }
-}
+    function search(){return Math.floor( (low + high)/2 );}       
+
+    
+    small.addEventListener('click', function(){
+        high = Number(answerF.innerHTML);
+        answerF.innerHTML = search();
+        numGuess++;  
+      });
+      
+      big.addEventListener('click', function(){
+        low = Number(answerF.innerHTML);    
+        answerF.innerHTML = search();
+        numGuess++;
+      });
+    
+    win.addEventListener("click", function(){
+        small.style.display = "none";
+        big.style.display = "none";
+        this.style.display = "none";
+        restart.style.display = "block";    
+        document.getElementById("winField").innerHTML = `Победа! угадал за ${numGuess +1} попыток!`;   
+      });
+
+    restart.addEventListener("click", function(){
+        location.reload();   
+      });
+     
+  })();
